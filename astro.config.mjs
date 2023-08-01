a3ff2eb4ea6d170a249e938708d5c2e0a660b40b
Rishi <rishivhavle21@gmail.com>
2023-08-01 13:36:01 +0530

    adapter cloudfalre

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import { SITE_URL } from "./src/data/config";
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: nodejs(),
+  adapter: cloudflare(),
   output: 'server',
   integrations: [tailwind(), sitemap()],
-  site: SITE_URL,
+  site: syncline.pages.dev,
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {
